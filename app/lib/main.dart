e44b49f379427e3acdef6c1601bb3f9950a11b62
William Gomes <113655914+Lionlaion369@users.noreply.github.com>
2025-09-23 07:56:55 -0400

    Update main.dart

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,1 +1,21 @@
+import 'package:flutter/material.dart';
+import 'screens/chat_screen.dart';
 
+void main() {
+  runApp(const WillaApp());
+}
+
+class WillaApp extends StatelessWidget {
+  const WillaApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Willa Assistant',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      home: const ChatScreen(),
+    );
+  }
+}
