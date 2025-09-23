8d5ff475bd6e615a1a3ea07bb16c886f5b216e83
William Gomes <113655914+Lionlaion369@users.noreply.github.com>
2025-09-23 11:28:46 -0400

    Update audio_service.dart
    Primeira alteração.

--- app/lib/services/audio_service.dart
+++ app/lib/services/audio_service.dart
@@ -1,1 +1,22 @@
+import 'package:speech_to_text/speech_to_text.dart' as stt;
+import 'package:flutter_tts/flutter_tts.dart';
 
+class AudioService {
+  final stt.SpeechToText speech = stt.SpeechToText();
+  final FlutterTts tts = FlutterTts();
+
+  Future<String?> listen() async {
+    bool available = await speech.initialize();
+    if (!available) return null;
+
+    await speech.listen();
+    await Future.delayed(const Duration(seconds: 5));
+    await speech.stop();
+
+    return speech.lastRecognizedWords;
+  }
+
+  Future<void> speak(String text) async {
+    await tts.speak(text);
+  }
+}
